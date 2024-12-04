a02c73d65056f7d70963318d785b09937ff0875f
adamjatim <adamrahmatilahi@gmail.com>
2024-12-04 21:12:27 +0700

    create 12 files, update 8 files and copy 1 file

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,28 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Head from 'next/head';
+import Header from "../components/Header";
+import Hero from "../components/Hero";
+import About from "../components/About";
+import Footer from "../components/Footer";
 
 export default function Home() {
   return (
-    <div className="container">
+    <div
+    // className="container"
+    >
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Kalong Project</title>
         <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Caveat:wght@400..700&family=DM+Mono:ital,wght@0,300;0,400;0,500;1,300;1,400;1,500&family=Redacted+Script:wght@300;400;700&display=swap" rel="stylesheet" />
+        <link href="https://fonts.cdnfonts.com/css/warung-kopi" rel="stylesheet" />
       </Head>
 
+      <Header />
+
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <Hero />
+        <About />
       </main>
 
       <Footer />
